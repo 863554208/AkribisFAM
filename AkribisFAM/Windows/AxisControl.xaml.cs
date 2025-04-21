31d690a5a26a5af9399fe40b563dd247746b6e1a
shiyy <shiyy@08-SHIYY>
2025-04-21 08:43:43 +0800

    添加项目文件。

---
+++ AkribisFAM/Windows/AxisControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AkribisFAM.Windows
+{
+    /// <summary>
+    /// AxisControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class AxisControl : UserControl
+    {
+        public AxisControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Axis_1_ControlButton_Click(object sender, RoutedEventArgs e)
+        {
+            AxisContorlDisplay.Content =  new Axis1ViewModel();
+        }
+
+        private void Axis_2_ControlButton_Click(object sender, RoutedEventArgs e)
+        {
+            AxisContorlDisplay.Content = new Axis2ViewModel();
+        }
+
+        private void Axis_3_ControlButton_Click(object sender, RoutedEventArgs e)
+        {
+            AxisContorlDisplay.Content = new Axis3ViewModel();
+        }
+    }
+}
---
+++ AkribisFAM/Windows/ManualControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AkribisFAM.Windows
+{
+    /// <summary>
+    /// ManualControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ManualControl : UserControl
+    {
+        public ManualControl()
+        {
+            InitializeComponent();
+        }
+
+        private void AxisConfigure_Click(object sender, RoutedEventArgs e)
+        {
+            // 将 ContentControl 显示的内容更改为 "主界面" 内容
+            ManualControlDisplay.Content = new AxisControl();  // MainScreen 是你定义的一个用户控件或界面
+        }
+
+        private void AirControl_Click(object sender, RoutedEventArgs e)
+        {
+            // 将 ContentControl 显示的内容更改为 "主界面" 内容
+            ManualControlDisplay.Content = new AirControl();  // MainScreen 是你定义的一个用户控件或界面
+        }
+    }
+}
