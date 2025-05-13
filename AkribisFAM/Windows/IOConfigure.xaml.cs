5587e30e41bf0af6b8b256370cdcd751bbb2e0de
qiuxg <qiuxg@14-QIUXG>
2025-05-13 09:38:02 +0800

    IO函数完善

--- AkribisFAM/Windows/IOConfigure.xaml.cs
+++ AkribisFAM/Windows/IOConfigure.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AkribisFAM.CommunicationProtocol;
 
 namespace AkribisFAM.Windows
 {
@@ -24,5 +26,49 @@ namespace AkribisFAM.Windows
         {
             InitializeComponent();
         }
+
+
+        //private void INOUTIOshow()
+        //{
+        //    Task.Run(new Action(() =>
+        //    {
+        //        while (true)
+        //        {
+        //            //显示在UI界面//输入
+        //            this.Label5.Dispatcher.BeginInvoke(new Action<bool>((IO_INstatus) =>
+        //            {
+        //                this.Label5.Background = IO_INstatus ? Brushes.Green : Brushes.Red;
+
+        //            }), IOManager.Instance.INIO_status[(int)IO_INFunction_Table.Stop_Sign1]);
+
+
+        //            this.Label9.Dispatcher.BeginInvoke(new Action<bool>((IO_INstatus) =>
+        //            {
+
+        //                this.Label9.Background = IO_INstatus ? Brushes.Green : Brushes.Red;
+        //            }), IOManager.Instance.INIO_status[(int)IO_INFunction_Table.NG_cover_plate1]);
+
+
+        //            //输出
+        //            this.Button9.Dispatcher.BeginInvoke(new Action<bool>((IO_OUTstatus) =>
+        //            {
+
+        //                this.Button9.Background = IO_OUTstatus ? Brushes.Green : Brushes.Red;
+        //            }), IOManager.Instance.OutIO_status[(int)IO_OutFunction_Table.Left_3_lift_cylinder_extend]);
+
+        //            this.Button11.Dispatcher.BeginInvoke(new Action<bool>((IO_OUTstatus) =>
+        //            {
+
+        //                this.Button11.Background = IO_OUTstatus ? Brushes.Green : Brushes.Red;
+        //            }), IOManager.Instance.OutIO_status[(int)IO_OutFunction_Table.Right_3_lift_cylinder_extend]);
+
+        //            Thread.Sleep(100);
+        //        }
+        //    }));
+        //}
+
+
+
+
     }
 }
