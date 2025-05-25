970807dfb216b8bb8b45c98365d1a2b4420b453f
wangxl <wangxl@akribis-sys.cn>
2025-05-25 20:36:45 +0800

    add new IP

--- AkribisFAM/Windows/InternetConfig.xaml.cs
+++ AkribisFAM/Windows/InternetConfig.xaml.cs
@@ -48,27 +48,33 @@ namespace AkribisFAM.Windows
         }
         private void Readdevicesjson()//读IP地址和端口号
         {
-            string filePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "devices.json");// 获取文件路径
-            string json = File.ReadAllText(filePath);// 读取JSON文件并反序列化为对象
-            JObject obj = JObject.Parse(json);
-            this.camera1_Feed_IP.Text = (obj["camera1_Feed"]["IP"]).ToString();
-            this.camera1_Feed_Port.Text = (obj["camera1_Feed"]["Port"]).ToString();
-            this.camera1_Runner_IP.Text = (obj["camera1_Runner"]["IP"]).ToString();
-            this.camera1_Runner_Port.Text = (obj["camera1_Runner"]["Port"]).ToString();
-            this.camera2_IP.Text = (obj["camera2"]["IP"]).ToString();
-            this.camera2_Port.Text = (obj["camera2"]["Port"]).ToString();
-            this.camera3_IP.Text = (obj["camera3"]["IP"]).ToString();
-            this.camera3_Port.Text = (obj["camera3"]["Port"]).ToString();
-            this.lazer_IP.Text = (obj["lazer"]["IP"]).ToString();
-            this.lazer_Port.Text = (obj["lazer"]["Port"]).ToString();
-            this.scanner_IP.Text = (obj["scanner"]["IP"]).ToString();
-            this.scanner_Port.Text = (obj["scanner"]["Port"]).ToString();
-            this.mes_IP.Text = (obj["mes"]["IP"]).ToString();
-            this.mes_Port.Text = (obj["mes"]["Port"]).ToString();
-            this.ModbusTCP_IP.Text = (obj["ModbusTCP"]["IP"]).ToString();
-            this.ModbusTCP_Port.Text = (obj["ModbusTCP"]["Port"]).ToString();
-            this.Pressure_sensor_IP.Text = (obj["Pressure_sensor"]["IP"]).ToString();
-            this.Pressure_sensor_Port.Text = (obj["Pressure_sensor"]["Port"]).ToString();
+            try
+            {
+                string filePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "devices.json");// 获取文件路径
+                string json = File.ReadAllText(filePath);// 读取JSON文件并反序列化为对象
+                JObject obj = JObject.Parse(json);
+                this.camera1_Feed_IP.Text = (obj["camera1_Feed"]["IP"]).ToString();
+                this.camera1_Feed_Port.Text = (obj["camera1_Feed"]["Port"]).ToString();
+                this.camera1_Runner_IP.Text = (obj["camera1_Runner"]["IP"]).ToString();
+                this.camera1_Runner_Port.Text = (obj["camera1_Runner"]["Port"]).ToString();
+                this.camera2_IP.Text = (obj["camera2"]["IP"]).ToString();
+                this.camera2_Port.Text = (obj["camera2"]["Port"]).ToString();
+                this.camera3_IP.Text = (obj["camera3"]["IP"]).ToString();
+                this.camera3_Port.Text = (obj["camera3"]["Port"]).ToString();
+                this.lazer_IP.Text = (obj["lazer"]["IP"]).ToString();
+                this.lazer_Port.Text = (obj["lazer"]["Port"]).ToString();
+                this.scanner_IP.Text = (obj["scanner"]["IP"]).ToString();
+                this.scanner_Port.Text = (obj["scanner"]["Port"]).ToString();
+                this.mes_IP.Text = (obj["mes"]["IP"]).ToString();
+                this.mes_Port.Text = (obj["mes"]["Port"]).ToString();
+                this.ModbusTCP_IP.Text = (obj["ModbusTCP"]["IP"]).ToString();
+                this.ModbusTCP_Port.Text = (obj["ModbusTCP"]["Port"]).ToString();
+                this.Pressure_sensor_IP.Text = (obj["Pressure_sensor"]["IP"]).ToString();
+                this.Pressure_sensor_Port.Text = (obj["Pressure_sensor"]["Port"]).ToString();
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Read device IP failed!");
+            }
         }
 
 
