d852ab9d6aa933770ef89e7fcfbe9466aa292118
raymondabqakribis <raymond.ang@akribis-sys.com>
2025-06-23 11:22:49 +0800

    Feat: Update TCP Client, added light control
    -Fix : Conveyor page digital input status not refresh bug

--- AkribisFAM/Properties/AssemblyInfo.cs
+++ AkribisFAM/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("0.0.0.8")]
-[assembly: AssemblyFileVersion("0.0.0.8")]
+[assembly: AssemblyVersion("0.0.0.10")]
+[assembly: AssemblyFileVersion("0.0.0.10")]
