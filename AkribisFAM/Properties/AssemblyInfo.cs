0a936b2730e53b1b48240366753585c815b19c80
raymondabqakribis <raymond.ang@akribis-sys.com>
2025-06-25 11:54:18 +0800

    Refact: Update version number

--- AkribisFAM/Properties/AssemblyInfo.cs
+++ AkribisFAM/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("0.0.0.14")]
-[assembly: AssemblyFileVersion("0.0.0.14")]
+[assembly: AssemblyVersion("0.0.0.15")]
+[assembly: AssemblyFileVersion("0.0.0.15")]
